042013b1bc07e5235c32dfef1a0ccb738a09f3b0
Bash <bashash10@gmail.com>
2024-02-19 23:06:23 +0000

    Template files
    these are all the files ready at hand

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Tailwind from "./Tailwind.svelte";
+</script>
+
+<div class=""></div>
+
+<h1 class="text-3xl font-thin">Welcome to Svelte (rollup) + TailwindCSS</h1>
+
+<Tailwind />
